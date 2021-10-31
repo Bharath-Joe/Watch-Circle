d8afde462bda392af3a9c595f5c06e87777b711c
Bharath-Joe <bsenthil@calpoly.edu>
2021-10-31 01:44:54 -0700

    Final Changes for Sprint 1

--- front-end/src/CreateAccount.js
+++ front-end/src/CreateAccount.js
@@ -32,34 +32,18 @@ function handleConfirmPassoword(event) {
 
   return ( 
     <form>
-      <center>
-        <h1> Create Account </h1> 
+      <center> 
+        <img src="https://docs.google.com/drawings/d/e/2PACX-1vR6EnoPyocFiar4BWVpGHrM3PE1Dcfa8lW-PpgsDoV87oGPFct9cH0P2igtEFJTq8qTPSABjCl1V9lC/pub?w=960&amp;h=720" alt=""width="250" height="240"/>
+        <h1> Create Account </h1>
       </center>
-      <label htmlFor="name">Username</label>
-      <input type="text"
-        name="name"
-        id="name"
-        value={person.name}
-        onChange={handleChange} />
-      <label htmlFor="password" >Password</label>
-      <input type="password"
-        name="password"
-        id="password"
-        value={person.password}
-        onChange={handleChange} 
-        style={{float : 'right'}}/>
-      <label htmlFor="confirm_password" >Confirm Password</label>
-      <input type="password"
-        name="confirm_password"
-        id="confirm_password"
-        value={person.confirm_password}
-        onChange={handleConfirmPassoword}/>
-      <center>
-        <input type="button" value="Sign Up" onClick={loggedIn} />
-      </center>
-      <center>
-        <input type="button" value="&#8592; Back to Log In" onClick={() => {history.push('/');}} />
-      </center>
+      <label htmlFor="name"> Username </label>
+      <input type="text" name="name" id="name" value={person.name} onChange={handleChange} />
+      <label htmlFor="password" > Password </label>
+      <input type="password" name="password" id="password" value={person.password} onChange={handleChange} style={{float : 'right'}}/>
+      <label htmlFor="confirm_password" > Confirm Password </label>
+      <input type="password" name="confirm_password" id="confirm_password" value={person.confirm_password} onChange={handleConfirmPassoword}/>
+      <center> <input type="button" value="Sign Up" onClick={loggedIn} /> </center>
+      <center> <input type="button" value="&#8592; Back to Log In" onClick={() => {history.push('/');}} /> </center>
     </form>
   );
 }
