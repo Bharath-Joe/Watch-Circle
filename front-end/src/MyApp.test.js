c6bd20ff331149bd4db69c5df638d2e26adca259
Bharath-Joe <bsenthil@calpoly.edu>
2021-11-05 13:42:25 -0700

    CI round 2

--- front-end/src/MyApp.test.js
+++ front-end/src/MyApp.test.js
@@ -4,7 +4,7 @@ import MyApp from "./MyApp";
 test("renders learn react link", () => {
   render(<MyApp />);
   // successful test:
-  const linkElement = screen.getByText(/Log In/i);
+  const linkElement = screen.getAllByText(/Log In/i);
   // failing test:
   // const linkElement = screen.getByText(/Painter/i);
   expect(linkElement).toBeInTheDocument();
