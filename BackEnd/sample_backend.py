d580c8577504c43b0f43f431b565f5472b2b4133
spencerwong20 <swong140@calpoly.edu>
2021-11-10 15:06:36 -0800

    Revert "Revert "Got log In to work with valid creds""
    This reverts commit 5bbb667b2bca4eaa4980aa9ea63729a83e76940e.

--- BackEnd/sample_backend.py
+++ BackEnd/sample_backend.py
@@ -11,10 +11,6 @@ from model_mongodb import User
 app = Flask(__name__)
 CORS(app)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-
 
 users = {
     'users_list':[]
@@ -24,15 +20,6 @@ users = {
 @app.route('/users', methods=['GET', 'POST'])
 def get_users():
     if request.method == 'GET':
-        # search_username = request.args.get('name')
-        # search_job = request.args.get('password')
-        # if search_username and search_job:
-        #     users = User().find_by_name_job(search_username, search_job)
-        # elif search_username:
-        #     users = User().find_by_name(search_username)
-        # elif search_job:
-        #     users = User().find_by_job(search_job)
-        # else:
         users = User().find_all()
         return {"users_list": users}
     if request.method == 'POST':
@@ -55,6 +42,10 @@ def get_user(username, password):
     if request.method == 'GET':
         user = User().find_by_name_password(username, password)
         if user:
-            return jsonify(user)
+            var1 = jsonify(user)
+            print(var1)
+            print("USer info: " + str(var1.data))
+            print("Status: " + str(var1.status))
+            return jsonify(user), 200
         else:
             return jsonify({"error": "User not found"}), 404
--- front-end/src/LogIn.js
+++ front-end/src/LogIn.js
@@ -6,35 +6,36 @@ import axios from 'axios';
 function LogIn(props){
     let history = useHistory();
 
-    const handleSubmit=(e) => {
-        const username=e.target.name.value;
-        const password=e.target.password.value;
+    async function checkLogIn(){
+        // const username=e.target.name.value;
+        // const password=e.target.password.value;
+        var username= document.getElementById("name").value;
+        var password= document.getElementById("password").value;
         console.log(username)
         console.log(password)
         try {
-            const response = axios.get('http://localhost:5000/users/' + username + '/' + password);
-            console.log(response.status)
-            alert("asdf")
-            // If status is 201 then go to a new page
-            // If status is 404 then display "Invalid Credentials"
+            const response = await axios.get('http://localhost:5000/users/' + username + '/' + password);
+            console.log(response.status);
+            if(response.status === 200){
+                history.push('/Shows');
+            }
           }
-          catch (error) {
+        catch (error) {
             console.log(error);
-            alert("Invalid Credentials")
+            alert("Invalid Credentials!");
             return false;
-          }
-      }
+        }
+    }
       
     return(
-        <form onSubmit={handleSubmit}>
+        <form>
             <center>
                 <img src="https://docs.google.com/drawings/d/e/2PACX-1vR6EnoPyocFiar4BWVpGHrM3PE1Dcfa8lW-PpgsDoV87oGPFct9cH0P2igtEFJTq8qTPSABjCl1V9lC/pub?w=960&amp;h=720" alt=""width="250" height="240"/>
                 <h2> Log In </h2>
             </center>
             <input type="text" name="name" id="name" placeholder="Username" size="10"/>
             <input type="password" name="password" id="password" placeholder="Password" width="20px"/>
-            <center> <button>Log In</button> </center>
-            
+            <center> <input type = "button" value = "Log In" onClick= {checkLogIn}/> </center>
             <center>
                 <input type="button" value="Create Account &#8594;" onClick={() => {history.push('/CreateAccount');
             }} />
