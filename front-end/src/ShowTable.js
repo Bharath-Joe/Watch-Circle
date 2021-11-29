e3c7fd25a10f35d51d3e857341f6a20073d1262d
yual-len-git <allenyu90@gmail.com>
2021-11-29 12:22:40 -0800

    changes

---
+++ front-end/src/ShowTable.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+function TableHeader() {
+	return (
+		<thead>
+			<tr>
+				<th>Show Name</th>
+				<th>Genre</th>
+				<th>Episode</th>
+				<th>Rating</th>
+				<th>Status</th>
+				<th>Delete</th>
+			</tr>
+		</thead>
+	);
+}
+
+function TableBody(props) {
+	const rows = props.characterData.map((row, index) => {
+		return (
+			<tr key={index}>
+				<td>{index}</td>
+				<td>{row.name}</td>
+				<td>{row.genre}</td>
+				<td>{row.episode}</td>
+				<td>{row.rating}</td>
+				<td>{row.status}</td>
+				<td>
+					<button onClick={() => props.removeCharacter(index)}>
+						Delete Show
+					</button>{" "}
+				</td>
+			</tr>
+		);
+	});
+	return <tbody>{rows}</tbody>;
+}
+
+function Table(props) {
+	return (
+		<table>
+			<TableHeader />
+			<TableBody
+				characterData={props.characterData}
+				removeCharacter={props.removeCharacter}
+			/>
+		</table>
+	);
+}
+
+export default Table;
