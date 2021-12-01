6dbeaf9358f59a4609b6d800b57bc082fd4831c2
spencerwong20 <swong140@calpoly.edu>
2021-12-01 15:22:28 -0800

    Attempt to test reload() method

--- BackEnd/test_sample_backend.py
+++ BackEnd/test_sample_backend.py
@@ -1,21 +1,21 @@
-import pytest
-import sample_backend
+# import pytest
+# import sample_backend
 
 
 # def test_find_users_by_name_success():
 #     expected = [
 #         {
-#             "_id": "61a6d4e02e4ccc8ab20f5929",
+#             "_id": "61a7ff342e46075d9b1c78ec",
 #             "confirm_password": "me",
 #             "name": "Spencer",
 #             "password": "me",
 #             "shows": [
 #                 {
-#                     "episode": "me",
-#                     "genre": "ness",
-#                     "name": "sad",
-#                     "rating": "is",
-#                     "status": "sad",
+#                     "episode": "1",
+#                     "genre": "w",
+#                     "name": "s",
+#                     "rating": "1",
+#                     "status": "C",
 #                     "user": "Spencer"
 #                 }
 #             ]
@@ -33,17 +33,17 @@ import sample_backend
 # def test_find_by_password_success():
 #     expected = [
 #         {
-#             "_id": "61a6d4e02e4ccc8ab20f5929",
+#             "_id": "61a7ff342e46075d9b1c78ec",
 #             "confirm_password": "me",
 #             "name": "Spencer",
 #             "password": "me",
 #             "shows": [
 #                 {
-#                     "episode": "me",
-#                     "genre": "ness",
-#                     "name": "sad",
-#                     "rating": "is",
-#                     "status": "sad",
+#                     "episode": "1",
+#                     "genre": "w",
+#                     "name": "s",
+#                     "rating": "1",
+#                     "status": "C",
 #                     "user": "Spencer"
 #                 }
 #             ]
@@ -58,101 +58,121 @@ import sample_backend
 #         "BobbyShmurda", "Huh") == expected
 
 
-# def test_find_all_success():
+# def test_reload_Success():
+#     user = sample_backend.User().find_by_name("Spencer")
+#     newUser = sample_backend.User(user[0])
+#     assert sample_backend.User.reload(newUser) == True
+
+
+# def test_reload_Fail():
+#     emptyDic = {}
+#     newUser = sample_backend.User(emptyDic)
+#     assert sample_backend.User.reload(newUser) == False
+
+
+# def test_addShow():
+
+#     addedShow = {
+#         "episode": "done",
+#         "genre": "with",
+#         "name": "this",
+#         "rating": "5",
+#         "status": "Ongoing",
+#         "user": "Spencer"
+#     }
+
 #     expected = [
 #         {
-#             "_id": "61a6d4e02e4ccc8ab20f5929",
+#             "_id": "61a7ff342e46075d9b1c78ec",
 #             "confirm_password": "me",
 #             "name": "Spencer",
 #             "password": "me",
 #             "shows": [
 #                 {
-#                     "episode": "me",
-#                     "genre": "ness",
-#                     "name": "sad",
-#                     "rating": "is",
-#                     "status": "sad",
+#                     "episode": "1",
+#                     "genre": "w",
+#                     "name": "s",
+#                     "rating": "1",
+#                     "status": "C",
 #                     "user": "Spencer"
+#                 }
+#             ]
+#         },
+#         {
+#             "_id": "61a8024865b8bd9a71da15e7",
+#             "confirm_password": "e",
+#             "name": "m",
+#             "password": "e",
+#             "shows": [
+#                 {
+#                     "episode": "done",
+#                     "genre": "with",
+#                     "name": "this",
+#                     "rating": "5",
+#                     "status": "Ongoing",
+#                     "user": "m"
+#                 },
+#                 {
+#                     "episode": "done",
+#                     "genre": "with",
+#                     "name": "this",
+#                     "rating": "5",
+#                     "status": "Ongoing",
+#                     "user": "m"
+#                 }
+#             ]
+#         }
+#     ]
+
+#     user = sample_backend.User().find_by_name("Spencer")
+#     newUser = sample_backend.User(user[0])
+#     # sample_backend.User.addShow(newUser, addedShow)
+
+#     # Checks to see if the show was added to the correct user and nowhere else
+#     # assert sample_backend.User().find_all() == expected
+
+
+# def test_find_all_success():
+#     expected = [
+#         {
+#             "_id": "61a7ff342e46075d9b1c78ec",
+#             "confirm_password": "me",
+#             "name": "Spencer",
+#             "password": "me",
+#             "shows": [
+#                 {
+#                     "episode": "1",
+#                     "genre": "w",
+#                     "name": "s",
+#                     "rating": "1",
+#                     "status": "C",
+#                     "user": "Spencer"
+#                 }
+#             ]
+#         },
+#         {
+#             "_id": "61a8024865b8bd9a71da15e7",
+#             "confirm_password": "e",
+#             "name": "m",
+#             "password": "e",
+#             "shows": [
+#                 {
+#                     "episode": "done",
+#                     "genre": "with",
+#                     "name": "this",
+#                     "rating": "5",
+#                     "status": "Ongoing",
+#                     "user": "m"
+#                 },
+#                 {
+#                     "episode": "done",
+#                     "genre": "with",
+#                     "name": "this",
+#                     "rating": "5",
+#                     "status": "Ongoing",
+#                     "user": "m"
 #                 }
 #             ]
 #         }
 #     ]
 #     assert sample_backend.User().find_all() == expected
-
-# def test_addShow():
-#     expected = [
-#         {
-#             "_id": "61a6d4e02e4ccc8ab20f5929",
-#             "confirm_password": "me",
-#             "name": "Spencer",
-#             "password": "me",
-#             "shows": []
-#         }
-#     ]
-#     assert sample_backend.Model().reload() == expected
-
-def test_find_users_by_name_success():
-    expected = [
-        {
-            "_id": "61a6c067028d80828f5536a0",
-            "confirm_password": "test2",
-            "name": "test2",
-            "password": "test2",
-            "shows": []
-        }
-    ]
-
-    assert sample_backend.User().find_by_name("test2") == expected
-
-
-def test_find_by_name_fail():
-    expected = []
-    assert sample_backend.User().find_by_name("BobbyShmurda") == expected
-
-
-def test_find_by_password_success():
-    expected = [
-        {
-            "_id": "61a6c067028d80828f5536a0",
-            "confirm_password": "test2",
-            "name": "test2",
-            "password": "test2",
-            "shows": []
-        }
-    ]
-    assert sample_backend.User().find_by_name_password("test2", "test2") == expected
-
-
-def test_find_by_password_fail():
-    expected = []
-    assert sample_backend.User().find_by_name_password(
-        "BobbyShmurda", "Huh") == expected
-
-
-def test_find_all_success():
-    expected = [
-        {
-            "_id": "61a6bfc3a567f06109cf232d",
-            "confirm_password": "test",
-            "name": "test",
-            "password": "test",
-            "shows": [
-                {
-                    "episode": "test",
-                    "genre": "test",
-                    "name": "test",
-                    "rating": "test",
-                    "status": "test",
-                    "user": "test"
-                }
-            ]
-        },
-        {
-            "_id": "61a6c067028d80828f5536a0",
-            "confirm_password": "test2",
-            "name": "test2",
-            "password": "test2",
-            "shows": []
-        }
-    ]
-    assert sample_backend.User().find_all() == expected
