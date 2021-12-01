95d855ff2b91f55210f6d9fd9775298a593fae23
spencerwong20 <swong140@calpoly.edu>
2021-12-01 11:34:34 -0800

    Fixed tests

--- BackEnd/test_sample_backend.py
+++ BackEnd/test_sample_backend.py
@@ -9,7 +9,16 @@ def test_find_users_by_name_success():
             "confirm_password": "me",
             "name": "Spencer",
             "password": "me",
-            "shows": []
+            "shows": [
+                {
+                    "episode": "me",
+                    "genre": "ness",
+                    "name": "sad",
+                    "rating": "is",
+                    "status": "sad",
+                    "user": "Spencer"
+                }
+            ]
         }
     ]
 
@@ -28,7 +37,16 @@ def test_find_by_password_success():
             "confirm_password": "me",
             "name": "Spencer",
             "password": "me",
-            "shows": []
+            "shows": [
+                {
+                    "episode": "me",
+                    "genre": "ness",
+                    "name": "sad",
+                    "rating": "is",
+                    "status": "sad",
+                    "user": "Spencer"
+                }
+            ]
         }
     ]
     assert sample_backend.User().find_by_name_password("Spencer", "me") == expected
@@ -47,7 +65,28 @@ def test_find_all_success():
             "confirm_password": "me",
             "name": "Spencer",
             "password": "me",
-            "shows": []
+            "shows": [
+                {
+                    "episode": "me",
+                    "genre": "ness",
+                    "name": "sad",
+                    "rating": "is",
+                    "status": "sad",
+                    "user": "Spencer"
+                }
+            ]
         }
     ]
     assert sample_backend.User().find_all() == expected
+
+# def test_reload():
+#     expected = [
+#         {
+#             "_id": "61a6d4e02e4ccc8ab20f5929",
+#             "confirm_password": "me",
+#             "name": "Spencer",
+#             "password": "me",
+#             "shows": []
+#         }
+#     ]
+#     assert sample_backend.Model().reload() == expected
